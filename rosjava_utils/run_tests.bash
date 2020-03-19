63360fc7c2433483b492491d761346e5a7a60b55
Michael McConnell <msmcconnell134@gmail.com>
2020-03-19 17:01:00 +0000

    Add rosjava_utils package

---
+++ rosjava_utils/run_tests.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+tests=(
+     'rosutils'
+   )
+for test in "${tests[@]}"
+do
+   echo "The current test is $test"
+   success=$(./gradlew :$test:test | grep 'BUILD SUCCESSFUL')
+   if [[ $success == "BUILD SUCCESSFUL" ]];
+   then
+      resultString=$'Success \n'
+      echo $resultString
+      echo ""
+      echo ""
+   else
+      resultString=$'Unsuccessful \n'
+      echo $resultString
+      echo ""
+      echo ""
+   fi
+done
+
