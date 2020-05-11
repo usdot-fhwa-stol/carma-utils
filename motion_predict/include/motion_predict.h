ef632596881bf5c8a8bbae6236f448ac2428e6d6
Aswath <32398753+aswath1@users.noreply.github.com>
2020-05-11 15:31:21 -0400

    Feature/cv (#36)

---
+++ motion_predict/include/motion_predict.h
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2019-2020 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+#ifndef MOTION_PREDICT_H
+#define MOTION_PREDICT_H
+
+#include <cav_msgs/ExternalObject.h>
+#include <cav_msgs/PredictedState.h>
+#include <cav_msgs/ExternalObjectList.h>
+#include "Eigen/Dense"
+#include <vector>
+
+namespace motion_predict{
+
+namespace cv{
+
+    /*! 
+    \brief  Mapping is used to map input range to an output range of different bandwidth.
+    \param  input is the current value of the process noise.
+    \param  process_noise_max is the maxium process noise of the system
+    */
+
+    double Mapping(const double input,const double process_noise_max);
+
+    /*! 
+    \brief  predictState is used to predict future state.
+    \param  pose is position and orientation (m).
+    \param  twist is velocity (m/s).
+    \param  delta_t time predicted into the future (sec).
+    */
+
+    cav_msgs::PredictedState predictState(const geometry_msgs::Pose& pose, const geometry_msgs::Twist& twist,const double delta_t);
+
+    /*! 
+    \brief  externalPredict populates motion prediction with future pose and velocity.
+    \param  obj external object.
+    \param  delta_t prediciton time into the future (sec)
+    \param  ax acceleration noise along x-axis (m^2/s^4)
+    \param  ay acceleration noise along y-axis (m^2/s^4)
+    \param  process_noise_max is the maximum process noise of the system
+    */
+
+    cav_msgs::PredictedState externalPredict(const cav_msgs::ExternalObject &obj,const double delta_t,const double ax,const double ay,const double process_noise_max);
+
+    /*! 
+    \brief  externalPeriod populates sequence of predicted motion of the object.
+    \param  obj external object.
+    \param  delta_t prediciton time into the future (sec)
+    \param  period sequence/time steps (sec)
+    \param  ax acceleration noise along x-axis (m^2/s^4)
+    \param  ay acceleration noise along y-axis (m^2/s^4)
+    \param  process_noise_max is the maximum process noise of the system
+    \param  confidence_drop_rate rate of drop in confidence with time
+    */
+
+    std::vector<cav_msgs::PredictedState> predictPeriod(const cav_msgs::ExternalObject& obj, const double delta_t, const double period,const double ax,const double ay ,const double process_noise_max,const double confidence_drop_rate);
+
+    /*! 
+    \brief  Mapping is used to map input range to an output range of different bandwidth.
+    \param  obj predicted object
+    \param  delta_t time predicted into the future (sec)
+    \param  confidence_drop_rate rate of drop in confidence with time
+    */
+
+    cav_msgs::PredictedState predictStep(const cav_msgs::PredictedState& obj, const double delta_t, const double confidence_drop_rate);
+   
+
+}//cv
+
+}//motion_predict
+
+#endif /* MOTION_PREDICT_H */
---
+++ motion_predict/src/predict_ctrv.cpp
@@ -0,0 +1,217 @@
+/*
+ * Copyright (C) 2019-2020 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+#include "predict_ctrv.h"
+#include "motion_predict.h"
+#include <math.h>
+#include "Eigen/Dense"
+
+
+namespace motion_predict
+{
+namespace ctrv
+{
+struct CTRV_State
+{
+  double x = 0;
+  double y = 0;
+  double yaw = 0;
+  double v = 0;  // magnitude of the speed
+  double yaw_rate = 0;
+};
+
+std::tuple<double, double> localVelOrientationAndMagnitude(const double v_x, const double v_y)
+{
+  Eigen::Vector2f x_y_vel(v_x, v_y);
+  double v_mag = x_y_vel.norm();
+
+  double local_v_orientation;  // The orientation of the velocity vector in the local object frame
+  if (fabs(v_mag) < 0.0000001)
+  {
+    local_v_orientation = 0;
+  }
+  else
+  {
+    local_v_orientation = asin(v_y / v_mag);
+  }
+  return std::make_tuple(local_v_orientation, v_mag);
+}
+
+CTRV_State buildCTRVState(const geometry_msgs::Pose& pose, const geometry_msgs::Twist& twist)
+{
+  geometry_msgs::Quaternion quat = pose.orientation;
+  Eigen::Quaternionf e_quat(quat.w, quat.x, quat.y, quat.z);
+  Eigen::Vector3f rpy = e_quat.toRotationMatrix().eulerAngles(0, 1, 2);
+
+  auto vel_angle_and_mag = localVelOrientationAndMagnitude(twist.linear.x, twist.linear.y);
+
+  CTRV_State state;
+  state.x = pose.position.x;
+  state.y = pose.position.y;
+  state.yaw =
+      rpy[2] + std::get<0>(vel_angle_and_mag);  // The yaw is relative to the velocity vector so take the heading and
+                                                // add it to the angle of the velocity vector in the local frame
+  state.v = std::get<1>(vel_angle_and_mag);
+  state.yaw_rate = twist.angular.z;
+
+  return state;
+}
+
+cav_msgs::PredictedState buildPredictionFromCTRVState(const CTRV_State& state, const geometry_msgs::Pose& original_pose,
+                                                      const geometry_msgs::Twist& original_twist)
+{
+  cav_msgs::PredictedState pobj;
+
+  // Map position
+  pobj.predicted_position.position.x = state.x;
+  pobj.predicted_position.position.y = state.y;
+  pobj.predicted_position.position.z = original_pose.position.z;
+
+  // Map orientation
+  Eigen::Quaternionf original_quat(original_pose.orientation.w, original_pose.orientation.x,
+                                   original_pose.orientation.y, original_pose.orientation.z);
+  Eigen::Vector3f original_rpy = original_quat.toRotationMatrix().eulerAngles(0, 1, 2);
+
+  auto vel_angle_and_mag = localVelOrientationAndMagnitude(original_twist.linear.x, original_twist.linear.y);
+
+  Eigen::Quaternionf final_quat;
+  final_quat = Eigen::AngleAxisf(original_rpy[0], Eigen::Vector3f::UnitX()) *
+               Eigen::AngleAxisf(original_rpy[1], Eigen::Vector3f::UnitY()) *
+               Eigen::AngleAxisf(state.yaw - std::get<0>(vel_angle_and_mag), Eigen::Vector3f::UnitZ());
+
+  pobj.predicted_position.orientation.x = final_quat.x();
+  pobj.predicted_position.orientation.y = final_quat.y();
+  pobj.predicted_position.orientation.z = final_quat.z();
+  pobj.predicted_position.orientation.w = final_quat.w();
+
+  // Map twist
+  // Constant velocity model means twist remains unchanged
+  pobj.predicted_velocity = original_twist;
+
+  return pobj;
+}
+
+CTRV_State CTRVPredict(const CTRV_State& state, const double delta_t)
+{
+  CTRV_State next_state;
+
+  // Handle divide by 0 case
+  if (fabs(state.yaw_rate) < 0.0000001)
+  {
+    next_state.x = state.x + state.v * cos(state.yaw) * delta_t;
+    next_state.y = state.y + state.v * sin(state.yaw) * delta_t;
+    next_state.yaw = state.yaw;
+    next_state.v = state.v;
+    next_state.yaw_rate = state.yaw_rate;
+
+    return next_state;
+  }
+
+  double v_w = state.v / state.yaw_rate;
+  double sin_yaw = sin(state.yaw);
+  double wT = state.yaw_rate * delta_t;
+
+  next_state.x = v_w * sin(wT + state.yaw) - v_w * sin_yaw + state.x;
+  next_state.y = -v_w * cos(wT + state.yaw) + v_w * sin_yaw + state.y;
+  next_state.yaw = wT + state.yaw;
+  next_state.v = state.v;
+  next_state.yaw_rate = state.yaw_rate;
+
+  return next_state;
+}
+
+// Forward predict an external object
+cav_msgs::PredictedState predictStep(const cav_msgs::ExternalObject& obj, const double delta_t,
+                                     const float process_noise_max, const double confidence_drop_rate)
+{
+  // Get initial state
+  CTRV_State state = buildCTRVState(obj.pose.pose, obj.velocity.twist);
+
+  // Predict Motion
+  CTRV_State next_state = CTRVPredict(state, delta_t);
+
+  // Convert CTRV to predicted state object
+  cav_msgs::PredictedState pobj = buildPredictionFromCTRVState(next_state, obj.pose.pose, obj.velocity.twist);
+
+  // Compute confidence values
+
+  double x_x = obj.pose.covariance[0];                   // X
+  double y_y = obj.pose.covariance[7];                   // Y
+  double yaw_yaw = obj.pose.covariance[35];              // Yaw
+  double vx_vx = obj.velocity.covariance[0];             // Vx
+  double vy_vy = obj.velocity.covariance[7];             // Vy
+  double yawrate_yawrate = obj.velocity.covariance[35];  // Yaw rate
+
+  // Average diagonal of process noise
+  double position_process_noise_avg = (x_x + y_y + yaw_yaw) / 3;
+  double velocity_process_noise_avg = (vx_vx + vy_vy + yawrate_yawrate) / 3;
+
+  // Map process noise average to confidence
+  pobj.predicted_position_confidence = motion_predict::cv::Mapping(position_process_noise_avg, process_noise_max) * confidence_drop_rate;
+  pobj.predicted_velocity_confidence = motion_predict::cv::Mapping(velocity_process_noise_avg, process_noise_max) * confidence_drop_rate;
+
+  // Update header
+  pobj.header = obj.header;
+  pobj.header.stamp += ros::Duration(delta_t);
+
+  return pobj;
+}
+
+// Forward predict a prediction
+cav_msgs::PredictedState predictStep(const cav_msgs::PredictedState& obj, const double delta_t,
+                                     const double confidence_drop_rate)
+{
+  // Get initial state
+  CTRV_State state = buildCTRVState(obj.predicted_position, obj.predicted_velocity);
+
+  // Predict Motion
+  CTRV_State next_state = CTRVPredict(state, delta_t);
+
+  // Convert CTRV to predicted state object
+  cav_msgs::PredictedState pobj =
+      buildPredictionFromCTRVState(next_state, obj.predicted_position, obj.predicted_velocity);
+
+  // Map process noise average to confidence
+  pobj.predicted_position_confidence = obj.predicted_position_confidence * confidence_drop_rate;
+  pobj.predicted_velocity_confidence = obj.predicted_velocity_confidence * confidence_drop_rate;
+
+  // Update header
+  pobj.header = obj.header;
+  pobj.header.stamp += ros::Duration(delta_t);
+
+  return pobj;
+}
+
+std::vector<cav_msgs::PredictedState> predictPeriod(const cav_msgs::ExternalObject& obj, const double delta_t,
+                                                    const double period, const float process_noise_max,
+                                                    const double confidence_drop_rate)
+{
+  std::vector<cav_msgs::PredictedState> predicted_states = { predictStep(obj, delta_t, process_noise_max,
+                                                                         confidence_drop_rate) };
+
+  double t = delta_t;
+  while (t < period)
+  {
+    predicted_states.emplace_back(predictStep(predicted_states.back(), delta_t, confidence_drop_rate));
+    t += delta_t;
+  }
+
+  return predicted_states;
+}
+
+}  // namespace ctrv
+
+}  // namespace motion_predict
