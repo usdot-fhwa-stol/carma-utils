9fffc9e6a04674eb3753e68f1b07102d97afb8f4
Isaiah <isaiah.d.lewis@leidos.com>
2021-12-06 18:45:13 +0000

    Commit PR fixes

--- bsm_helper/src/bsm_helper.cpp
+++ bsm_helper/src/bsm_helper.cpp
@@ -1,3 +1,13 @@
+/* Copyright (C) 2021 LEIDOS.
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ use this file except in compliance with the License. You may obtain a copy of
+ the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ License for the specific language governing permissions and limitations under
+ the License.
+*/
 #include "bsm_helper/bsm_helper.h"
 
 namespace BSMHelper{
@@ -5,19 +15,16 @@ namespace BSMHelper{
 
     std::string BSMHelper::bsmIDtoString(std::vector<uint8_t, std::allocator<uint8_t>> id)
     {
-
-        std::string id_test;
-
-        for(size_t i =0; i < id.size();i++)
+        if(id.size() > 4)
         {
-            id_test.append(std::to_string(id.at(i)));
+            throw std::invalid_argument("Invalid id value");
         }
 
-        std::stringstream int_id(id_test);
-
-        unsigned long id_num;
-
-        int_id >> id_num;
+        unsigned long id_num = 0;
+        for(size_t i = 0; i < id.size(); i++)
+        {
+          id_num = (id_num * pow(10, 2)) + id.at(i) ;
+        }
 
 
         std::stringstream converted;
