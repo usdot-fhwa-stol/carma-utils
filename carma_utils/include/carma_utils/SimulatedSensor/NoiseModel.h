49d9ceba8cea1d8de0842aeb9c8d22afd9737f92
david <david.gayman@leidos.com>
2023-07-17 12:43:24 -0400

    Adding classes.

---
+++ carma_utils/include/carma_utils/SimulatedSensor/NoiseModel.h
@@ -0,0 +1,18 @@
+//
+// Created by stol on 7/17/23.
+//
+
+#ifndef CARMA_UTILS_NOISEMODEL_H
+#define CARMA_UTILS_NOISEMODEL_H
+
+namespace carma_utils {
+    namespace sensor {
+
+        class NoiseModel {
+
+        };
+
+    }
+}
+
+#endif //CARMA_UTILS_NOISEMODEL_H
---
+++ carma_utils/src/carma_utils/SimulatedSensor/SensedObject.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stol on 7/17/23.
+//
+
+#include "SensedObject.h"
