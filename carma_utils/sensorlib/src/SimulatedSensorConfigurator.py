608b158d1c1bd57b69b71328e16e79ca58c322f2
david <david.gayman@leidos.com>
2023-08-21 14:51:08 -0400

    Simplified configurator interface, per TODOs.

--- carma_utils/sensorlib/src/SimulatedSensorConfigurator.py
+++ carma_utils/sensorlib/src/SimulatedSensorConfigurator.py
@@ -17,50 +17,38 @@ from src.util.SimulatedSensorUtils import SimulatedSensorUtils
 class SimulatedSensorConfigurator:
     """
     Interface to build a SimulatedSensor.
-
-    Configuration steps:
-    1. SimulatedSensor settings
-    2. CARLA sensor settings from either:
-        a) Existing spawned sensor
-        b) Existing blueprint
-        c) Configuration only
-    3. Noise model
     """
 
-    def __init__(self):
-        SimulatedSensorConfigurator.infrastructure_sensors = {}
-        SimulatedSensorConfigurator.client = None
-        SimulatedSensorConfigurator.carla_world = None
+    # Static fields
+    infrastructure_sensors = {}
+    client = None
+    carla_world = None
 
-    # TODO load config from file
-
-    # TODO Function to build and register a sensor
-    #  def register_sensor(sensor_position, sensor_orientation, [sensor_transform],
-    #  config dictionary)
-
-    # TODO Retrieve data function
-    ## TODO simplify this to require no world, a transform, optional parentobejct, infrasture ID (store it inside an attributes dictionary or something, and possibly keep up-to-date DetectedObject cache), simulate
+    # ------------------------------------------------------------------------------
+    # SimulatedSensor Management Interface
+    # ------------------------------------------------------------------------------
 
     @staticmethod
-    def build_simulated_semantic_lidar_sensor(simulated_sensor_config, carla_sensor_config,
-                                              noise_model_config,
-                                              infrastructure_id, transform, parent_object=None):
+    def register_simulated_semantic_lidar_sensor(simulated_sensor_config, carla_sensor_config, noise_model_config,
+                                              infrastructure_id, sensor_transform, parent_actor=None):
         """
         Builds a SemanticLidarSensor from a CARLA Semantic LIDAR Sensor.
-        :return:
+        :param simulated_sensor_config: The configuration for the simulated sensor.
+        :param carla_sensor_config: The configuration for the CARLA sensor.
+        :param noise_model_config: The configuration for the noise model.
+        :param infrastructure_id: The ID of the infrastructure.
+        :param sensor_transform: The transform of the sensor.
+        :param parent_actor: The parent actor of the sensor (optional).
+        :return: A registered SemanticLidarSensor.
         """
 
-        # Initialize the CARLA connection, which only needs to be done once
-        if SimulatedSensorConfigurator.client is None:
-            SimulatedSensorConfigurator.client = carla.Client(simulated_sensor_config["carla_connection"]["carla_host"],
-                                                              simulated_sensor_config["carla_connection"]["carla_port"])
-            SimulatedSensorConfigurator.client.set_timeout(2.0)
-            SimulatedSensorConfigurator.carla_world = SimulatedSensorConfigurator.client.get_world()
-
+        carla_world = SimulatedSensorConfigurator.__get_initialized_carla_world(simulated_sensor_config)
         carla_world = SimulatedSensorConfigurator.carla_world
 
         # Retrieve the CARLA sensor
-        carla_sensor = None
+        blueprint_library = carla_world.get_blueprint_library()
+        sensor_bp = SimulatedSensorConfigurator.__generate_lidar_bp(blueprint_library, carla_sensor_config)
+        carla_sensor = carla_world.spawn_actor(sensor_bp, sensor_transform, attach_to=parent_actor)
 
         # Build internal objects
         sensor = CarlaSensorBuilder.build_sensor(carla_sensor)
@@ -71,10 +59,42 @@ class SimulatedSensorConfigurator:
         simulated_sensor = SemanticLidarSensor(infrastructure_id, simulated_sensor_config, carla_sensor_config,
                                                carla_world, sensor,
                                                data_collector, noise_model)
+
+        # Register the sensor for fast retrieval
         SimulatedSensorConfigurator.infrastructure_sensors[infrastructure_id] = simulated_sensor
-        return SemanticLidarSensor(simulated_sensor_config, carla_sensor_config, carla_world, sensor, data_collector,
-                                   noise_model)
+
+        return SemanticLidarSensor(infrastructure_id, simulated_sensor_config, carla_sensor_config, carla_world, sensor,
+                                   data_collector, noise_model)
 
     @staticmethod
     def get_simulated_sensor(infrastructure_id):
+        """Retrieve a SimulatedSensor."""
         return SimulatedSensorConfigurator.infrastructure_sensors.get(infrastructure_id)
+
+    # ------------------------------------------------------------------------------
+    # Helper Functions
+    # ------------------------------------------------------------------------------
+
+    @staticmethod
+    def __get_initialized_carla_world(simulated_sensor_config):
+        """Initialize the CARLA connection, which only needs to be done once."""
+
+        if SimulatedSensorConfigurator.client is None:
+            SimulatedSensorConfigurator.client = carla.Client(simulated_sensor_config["carla_connection"]["carla_host"],
+                                                              simulated_sensor_config["carla_connection"]["carla_port"])
+            SimulatedSensorConfigurator.client.set_timeout(2.0)
+            SimulatedSensorConfigurator.carla_world = SimulatedSensorConfigurator.client.get_world()
+
+        return SimulatedSensorConfigurator.carla_world
+
+    @staticmethod
+    def __generate_lidar_bp(blueprint_library, carla_sensor_config):
+        """Build the CARLA blueprint necessary for CARLA sensor construction."""
+        lidar_bp = blueprint_library.find("sensor.lidar.ray_cast_semantic")
+        lidar_bp.set_attribute("upper_fov", str(carla_sensor_config["upper_fov"]))
+        lidar_bp.set_attribute("lower_fov", str(carla_sensor_config["lower_fov"]))
+        lidar_bp.set_attribute("channels", str(carla_sensor_config["channels"]))
+        lidar_bp.set_attribute("range", str(carla_sensor_config["range"]))
+        lidar_bp.set_attribute("rotation_frequency", str(1.0 / carla_sensor_config["rotation_period"]))
+        lidar_bp.set_attribute("points_per_second", str(carla_sensor_config["points_per_second"]))
+        return lidar_bp
