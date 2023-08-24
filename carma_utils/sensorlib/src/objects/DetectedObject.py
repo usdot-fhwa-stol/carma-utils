f3aece5bc0e465cb9398702d2073a0803250be05
david <david.gayman@leidos.com>
2023-08-24 14:59:59 -0400

    Formatting and cleanup.

--- carma_utils/sensorlib/src/objects/DetectedObject.py
+++ carma_utils/sensorlib/src/objects/DetectedObject.py
@@ -6,8 +6,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 
-# !/usr/bin/python
-# -*- coding: utf-8 -*-
 from dataclasses import dataclass
 from typing import Tuple, List
 
--- carma_utils/sensorlib/test/integration/simulated_sensor_open3d_lidar.py
+++ carma_utils/sensorlib/test/integration/simulated_sensor_open3d_lidar.py
@@ -1,3 +1,5 @@
+# !/usr/bin/env python
+
 # Copyright (C) 2021 LEIDOS.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
@@ -5,9 +7,7 @@
 # applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
-
-# !/usr/bin/env python
-
+#
 # Copyright (c) 2020 Computer Vision Center (CVC) at the Universitat Autonoma de
 # Barcelona (UAB).
 #
