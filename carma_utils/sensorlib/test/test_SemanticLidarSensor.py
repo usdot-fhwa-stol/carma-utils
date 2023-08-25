f223a30a1d1cff5e5f0924deff2c395cbea1f61a
david <david.gayman@leidos.com>
2023-08-25 12:31:05 -0400

    Added serialized json data in test file.

--- carma_utils/sensorlib/test/test_SemanticLidarSensor.py
+++ carma_utils/sensorlib/test/test_SemanticLidarSensor.py
@@ -11,6 +11,7 @@ from dataclasses import replace
 from unittest.mock import MagicMock
 
 import carla
+import json
 import numpy as np
 
 from src.SemanticLidarSensor import SemanticLidarSensor
@@ -18,6 +19,7 @@ from src.collector.SensorDataCollector import SensorDataCollector
 from src.noise_models.GaussianNoiseModel import GaussianNoiseModel
 from src.objects.CarlaSensor import CarlaSensorBuilder
 from src.objects.DetectedObject import DetectedObjectBuilder
+from src.util.CarlaUtils import CarlaUtils
 from test.util.SimulatedSensorTestUtils import SimulatedSensorTestUtils
 
 
@@ -105,9 +107,22 @@ class TestSemanticLidarSensor(unittest.TestCase):
         self.sensor.update_object_metadata.assert_called_once_with(detected_objects)
 
         self.assertEqual(result, detected_objects)
+        self.assertEqual(self.sensor._SemanticLidarSensor__detected_objects, detected_objects)
 
     def test_get_detected_objects_json(self):
-        assert False
+        detected_objects = SimulatedSensorTestUtils.generate_test_data_detected_objects()
+        detected_objects = [replace(obj, carla_actor=None) for obj in detected_objects]
+        self.sensor._SemanticLidarSensor__detected_objects = detected_objects
+        serialized  = self.sensor.get_detected_objects_json()
+        with open("data/test_data_serialized_detected_objects.json", "r") as file:
+            expected_serialized_data = json.load(file)
+            assert serialized == expected_serialized_data
+
+
+
+
+
+
 
     def test_get_scene_detected_objects(self):
         actors = [MagicMock()]
@@ -212,23 +227,6 @@ class TestSemanticLidarSensor(unittest.TestCase):
         assert expected_threshold == threshold
 
     def test_apply_occlusion(self):
-        detected_object = MagicMock(id=1)
-        actor_angular_extents = {1: (0.0, 1.096)}
-        hitpoints = {1: []}
-        detection_thresholds = {1: 0.5}
-
-        # Mock internal function
-        self.sensor.is_visible = MagicMock(return_value=detected_object)
-
-        # Call and apply assertions
-        self.sensor.apply_occlusion([detected_object], actor_angular_extents, hitpoints, detection_thresholds)
-
-        self.sensor.is_visible.assert_called_with(actor_angular_extents[detected_object.id],
-                                                  hitpoints[detected_object.id],
-                                                  detection_thresholds[detected_object.id]
-                                                  )
-
-    def test_apply_occlusion(self):
         # Specify inputs
         detected_objects = SimulatedSensorTestUtils.generate_test_data_detected_objects()
         actor_angular_extents = {
@@ -258,58 +256,50 @@ class TestSemanticLidarSensor(unittest.TestCase):
 
     def test_is_visible(self):
 
-        carla_sensor = MagicMock(points_per_second=10000, rotation_frequency=10, horizontal_fov=1.096,
-                                 vertical_fov=1.096, number_of_channels=10)
-        self.sensor._SemanticLidarSensor__sensor = carla_sensor
-        fov = 1.096
-        detection_threshold_ratio = 0.5
 
-        # Test case 1: Number of hitpoints is greater than or equal to the minimum required
-        object_hitpoints = []
-        for i in range(4000):
-            object_hitpoints += [MagicMock()]
-        result = self.sensor.is_visible((fov, fov), object_hitpoints, detection_threshold_ratio)
+        # Problem setup
+        num_horizontal_points_per_scan=360
+        num_vertical_points_per_scan=60
+
+        horizontal_fov = np.deg2rad(15)
+        vertical_fov = np.deg2rad(10)
+
+        sensor_horizontal_fov = np.deg2rad(360)
+        sensor_vertical_fov = np.deg2rad(60)
+
+        rotation_frequency = 1
+        points_per_second = num_horizontal_points_per_scan * rotation_frequency
+
+        # Mock the carla sensor
+        carla_sensor = MagicMock(points_per_second=points_per_second, rotation_frequency=rotation_frequency, horizontal_fov=sensor_horizontal_fov,
+                                 vertical_fov=sensor_vertical_fov, number_of_channels=num_vertical_points_per_scan)
+        self.sensor._SemanticLidarSensor__sensor = carla_sensor
+
+        # Object dimensions
+        actor_angular_extents = (horizontal_fov, vertical_fov)
+        detection_threshold_ratio = 0.7
+        expected_threshold_value = 105
+
+        # Test case 1: Number of hitpoints is greater than the minimum required
+        object_hitpoints = [MagicMock() for _ in range(10 * expected_threshold_value)]
+        result = self.sensor.is_visible(actor_angular_extents, object_hitpoints, detection_threshold_ratio)
+        self.assertTrue(result)
+
+        # Test case 2: Number of hitpoints is equal to the minimum required
+        object_hitpoints = [MagicMock() for _ in range(expected_threshold_value)]
+        result = self.sensor.is_visible(actor_angular_extents, object_hitpoints, detection_threshold_ratio)
         self.assertTrue(result)
 
         # Test case 2: Number of hitpoints is less than the minimum required
-        object_hitpoints = []
-        for i in range(4):
-            object_hitpoints += [MagicMock()]
-        result = self.sensor.is_visible((fov, fov), object_hitpoints, detection_threshold_ratio)
+        object_hitpoints = [MagicMock() for _ in range(int(0.5 * expected_threshold_value))]
+        result = self.sensor.is_visible(actor_angular_extents, object_hitpoints, detection_threshold_ratio)
         self.assertFalse(result)
 
+
+
+
+
     def test_compute_expected_num_hitpoints(self):
-        carla_sensor = MagicMock(points_per_second=10000, rotation_frequency=10, fov_angular_width=1.096)
-        self.sensor._SemanticLidarSensor__sensor = carla_sensor
-
-        fov = 1.096
-        num_points_per_scan = carla_sensor.points_per_second / carla_sensor.rotation_frequency
-        theta_resolution = carla_sensor.fov_angular_width / num_points_per_scan
-        expected_result = fov / theta_resolution
-
-        result = self.sensor.compute_expected_num_hitpoints(fov)
-
-        self.assertEqual(result, expected_result)
-
-
-
-    def test_is_visible(self):
-
-
-
-        .7
-        105
-
-
-
-
-
-
-
-
-
-
-    def test_compute_expected_num_vertical_hitpoints(self):
 
         # Problem setup
         num_horizontal_points_per_scan=360
@@ -356,11 +346,61 @@ class TestSemanticLidarSensor(unittest.TestCase):
         noise_model.apply_list_inclusion_noise.assert_called_once_with(detected_objects)
 
     def test_update_object_metadata(self):
+        original_type = "Vehicles"
+        expected_type = "Bridge"
+
+        hitpoints = dict([(i, [MagicMock(object_tag=CarlaUtils.get_semantic_tag_id(expected_type))]) for i in range(0, 6)])
+        timestamp = 3
+
+        # Generate objects in original world frame
+        detected_objects = SimulatedSensorTestUtils.generate_test_data_detected_objects()
+        detected_objects = [
+            replace(detected_objects[0], object_type=original_type, timestamp=0, position=np.array([10.0, 10.0, 0.0])),
+            replace(detected_objects[1], object_type=original_type, timestamp=0, position=np.array([11.0, 9.0, 0.0])),
+            replace(detected_objects[2], object_type=original_type, timestamp=0, position=np.array([14.0, 9.0, 0.0])),
+            replace(detected_objects[3], object_type=original_type, timestamp=0, position=np.array([8.0, 10.0, 0.0])),
+            replace(detected_objects[4], object_type=original_type, timestamp=0, position=np.array([8.0, 14.0, 0.0])),
+            replace(detected_objects[5], object_type=original_type, timestamp=0, position=np.array([14.0, 14.0, 0.0]))
+        ]
+
+        # Execute
+        new_detected_objects = self.sensor.update_object_metadata(detected_objects, hitpoints, timestamp)
+
+        # Assert object types updated
+        assert new_detected_objects[0].object_type == "Bridge"
+        assert new_detected_objects[1].object_type == "Bridge"
+        assert new_detected_objects[2].object_type == "Bridge"
+        assert new_detected_objects[3].object_type == "Bridge"
+        assert new_detected_objects[4].object_type == "Bridge"
+        assert new_detected_objects[5].object_type == "Bridge"
+
+        # Assert timestamps updated
+        assert new_detected_objects[0].timestamp == timestamp
+        assert new_detected_objects[1].timestamp == timestamp
+        assert new_detected_objects[2].timestamp == timestamp
+        assert new_detected_objects[3].timestamp == timestamp
+        assert new_detected_objects[4].timestamp == timestamp
+        assert new_detected_objects[5].timestamp == timestamp
+
+        # Assert the objects have been translated to the sensor-centric frame
+        assert np.allclose(new_detected_objects[0].position, np.array([9.0, 9.0, 0.0]))
+        assert np.allclose(new_detected_objects[1].position, np.array([10.0, 8.0, 0.0]))
+        assert np.allclose(new_detected_objects[2].position, np.array([13.0, 8.0, 0.0]))
+        assert np.allclose(new_detected_objects[3].position, np.array([7.0, 9.0, 0.0]))
+        assert np.allclose(new_detected_objects[4].position, np.array([7.0, 13.0, 0.0]))
+        assert np.allclose(new_detected_objects[5].position, np.array([13.0, 13.0, 0.0]))
+
+
+
+
+
+    def test_update_object_metadata_from_hitpoint(self):
+
         # Build mock objects
-        expected_type = "CorrectedVehicle"
+        expected_type = 15
         carla_actor = MagicMock()
         carla_actor.id = 0
-        carla_actor.semantic_tags = ["Vehicles"]
+        carla_actor.semantic_tags = [10]
         carla_actor.get_world_vertices = MagicMock(return_value=[carla.Location(1.0, 2.0, 3.0),
                                                                  carla.Location(4.0, 5.0, 6.0),
                                                                  carla.Location(7.0, 8.0, 9.0),
@@ -374,38 +414,9 @@ class TestSemanticLidarSensor(unittest.TestCase):
 
         hitpoints = {0: [MagicMock(object_tag=expected_type)]}
 
+        timestamp = 3
+
         # Call and provide assertions
-        corrected_objects = self.sensor.update_object_types([detected_object], hitpoints)
-        assert expected_type == corrected_objects[0].object_type
-
-    def test_update_object_metadata_from_hitpoint(self):
-        original_type = "Vehicles"
-        expected_type = "CorrectedVehicle"
-
-        detected_object = MagicMock(id=0, object_type=original_type)
-        hitpoints = {0: [MagicMock(object_tag=expected_type)]}
-
-        actual_type = self.sensor.get_object_type_from_hitpoint(detected_object, hitpoints)
-        self.assertEqual(expected_type, actual_type)
-
-        # Generate objects in original world frame
-        detected_objects = SimulatedSensorTestUtils.generate_test_data_detected_objects()
-        detected_objects = [
-            replace(detected_objects[0], position=np.array([10.0, 10.0, 0.0])),
-            replace(detected_objects[1], position=np.array([11.0, 9.0, 0.0])),
-            replace(detected_objects[2], position=np.array([14.0, 9.0, 0.0])),
-            replace(detected_objects[3], position=np.array([8.0, 10.0, 0.0])),
-            replace(detected_objects[4], position=np.array([8.0, 14.0, 0.0])),
-            replace(detected_objects[5], position=np.array([14.0, 14.0, 0.0]))
-        ]
-
-        # Execute
-        new_detected_objects = self.sensor.transform_to_sensor_frame(detected_objects)
-
-        # Assert the objects have been translated to the sensor-centric frame
-        assert np.allclose(new_detected_objects[0].position, np.array([9.0, 9.0, 0.0]))
-        assert np.allclose(new_detected_objects[1].position, np.array([10.0, 8.0, 0.0]))
-        assert np.allclose(new_detected_objects[2].position, np.array([13.0, 8.0, 0.0]))
-        assert np.allclose(new_detected_objects[3].position, np.array([7.0, 9.0, 0.0]))
-        assert np.allclose(new_detected_objects[4].position, np.array([7.0, 13.0, 0.0]))
-        assert np.allclose(new_detected_objects[5].position, np.array([13.0, 13.0, 0.0]))
+        corrected_objects = self.sensor.update_object_metadata_from_hitpoint(detected_object, hitpoints.get(detected_object.id), timestamp)
+        assert "Bridge" == corrected_objects.object_type
+        assert timestamp == corrected_objects.timestamp
