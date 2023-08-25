9fdea6bd546509ce53a7410ab205ff5e9658faae
david <david.gayman@leidos.com>
2023-08-25 13:20:28 -0400

    Main file partial

--- carma_utils/sensorlib/src/main.py
+++ carma_utils/sensorlib/src/main.py
@@ -1,11 +1,18 @@
 from datetime import time
 
 from src.SimulatedSensorConfigurator import SimulatedSensorConfigurator
+from src.util.SimulatedSensorUtils import SimulatedSensorUtils
 
 if __name__ == "__main__":
 
     # Build sensor
-    simulated_lidar_sensor = SimulatedSensorConfigurator.register_simulated_semantic_lidar_sensor()
+    config = SimulatedSensorUtils.load_config_from_file("simulated_sensor_config.yaml")
+    simulated_sensor_config = config["simulated_sensor"]
+    carla_sensor_config = config["lidar_sensor"]
+    noise_model_config = SimulatedSensorUtils.load_config_from_file("nosie_model_config.yaml")
+
+simulated_lidar_sensor = SimulatedSensorConfigurator.register_simulated_semantic_lidar_sensor(simulated_sensor_config, carla_sensor_config, noise_model_config,
+                                                                                                  infrastructure_id, sensor_transform, None)
 
     # Compute detected objects continuously
     while True:
