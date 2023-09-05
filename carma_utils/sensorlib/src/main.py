b25ad78127dc90787a0ae8932c8987cfb00ccb93
David Gayman <123024103+davidgayman@users.noreply.github.com>
2023-09-05 16:07:26 -0400

    VRU Simulated Sensor CDAR-158 (#140)
    <!-- Thanks for the contribution, this is awesome. -->
    
    # PR Details
    
    ## Description
    
    The SimulatedSensor acts as a wrapper to the CARLA Semantic Lidar
    Sensor, capturing the data and performing post-processing to generate a
    list of detected objects upon request.
    
    The SimulatedSesnsor.get_detected_objects_in_frame() function presents
    the primary processing sequence. Calls to internal utility classes build
    DetectedObject's, representing the detected vulnerable road users.
    High-speed LIDAR data is collected into a DataCollector class running in
    a separate context which self-populates via a callback.
    
    The processing sequence performs the following stages:
    1. Truth state retrieval.
    2. Prefilter by distance from sensor.
    3. LIDAR hitpoint retrieval.
    4. Occlusion determination using a fractional hitpoint coverage
    threshold scaling proportionally with the sensor-to-object distance.
    5. Application of a generic noise model to the results.
    
    Processing frames are understood to consist of one full LIDAR sensor
    rotation, and may include vertical scan elements.
    
    Configurability is provided to the SimulatedSensor, utility functions
    computing prefiltering and occlusion threshold scaling, and the noise
    model. Furthermore the noise model is extensible.
    
    ## Related Jira Key
    
    [CDAR-158](https://usdot-carma.atlassian.net/browse/CDAR-158)
    
    ## Motivation and Context
    
    This new feature is provided to enable vulnerable road user detection
    using a LIDAR sensor in the context of a CARLA-driven simulation. The
    data is intended to feed into a CARMA V2X system for data fusion with
    other sensors and action by CARMA platform vehicles.
    
    ## How Has This Been Tested?
    
    Code is being unit tested, and integration testing in
    test/integration/simulated_sensor_open3d_lidar.py successfully collects
    data into the internal DataCollector and performs cache rotation.
    
    Further testing is required before release.
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Types of changes
    
    - [ ] Defect fix (non-breaking change that fixes an issue)
    - [x ] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that cause existing functionality
    to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have added any new packages to the sonar-scanner.properties file
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have read the
    [**CONTRIBUTING**](https://github.com/usdot-fhwa-stol/carma-platform/blob/develop/Contributing.md)
    document.
    - [x ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    
    
    [CDAR-158]:
    https://usdot-carma.atlassian.net/browse/CDAR-158?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ carma_utils/sensorlib/src/main.py
@@ -0,0 +1,73 @@
+import argparse
+import json
+import time
+
+import carla
+
+from src.SimulatedSensorConfigurator import SimulatedSensorConfigurator
+from src.util.SimulatedSensorUtils import SimulatedSensorUtils
+
+if __name__ == "__main__":
+
+    # Parse arguments
+    argparser = argparse.ArgumentParser(
+        description=__doc__)
+
+    argparser.add_argument(
+        "--id",
+        default=-1,
+        type=int,
+        help="Infrastructure ID to assign to the sensor. Negative value forces auto-assignment. (default: -1)")
+
+    argparser.add_argument(
+        "--detection-cycle-delay-seconds",
+        default=0.5,
+        type=float,
+        help="Delay in continuous sensor processing loop in seconds. (default: 0.5)")
+
+    argparser.add_argument(
+        "--sensor-config",
+        default="",
+        type=str,
+        help="Sensor configuration as JSON dictionary. (default: \"\")")
+
+    argparser.add_argument(
+        "--noise-model-config",
+        default="",
+        type=str,
+        help="Noise model configuration as JSON dictionary. (default: \"\")")
+
+    args = argparser.parse_args()
+
+    # Get inputs
+    infrastructure_id = args.id
+    detection_cycle_delay_seconds = args.detection_cycle_delay_seconds
+    sensor_transform = carla.Transform(carla.Location(x=0.0, y=0.0, z=0.0),
+                                       carla.Rotation(pitch=0.0, yaw=0.0, roll=0.0))
+
+    # Build configuration
+    if args.sensor_config == "":
+        config = SimulatedSensorUtils.load_config_from_file("../config/simulated_sensor_config.yaml")
+    else:
+        config = json.loads(args.sensor_config)
+    simulated_sensor_config = config["simulated_sensor"]
+    carla_sensor_config = config["lidar_sensor"]
+
+    if args.noise_model_config == "":
+        noise_model_config = SimulatedSensorUtils.load_config_from_file("../config/noise_model_config.yaml")
+    else:
+        noise_model_config = json.loads(args.noise_model_config)
+
+    # Build sensor
+    simulated_lidar_sensor = SimulatedSensorConfigurator.register_simulated_semantic_lidar_sensor(
+        simulated_sensor_config,
+        carla_sensor_config,
+        noise_model_config,
+        infrastructure_id,
+        sensor_transform,
+        None)
+
+    # Compute detected objects continuously
+    while True:
+        simulated_lidar_sensor.compute_detected_objects()
+        time.sleep(detection_cycle_delay_seconds)
---
+++ carma_utils/sensorlib/test/objects/test_CarlaSensor.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2023 LEIDOS.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+# applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
+
+import unittest
+from unittest.mock import MagicMock
+
+import carla
+import numpy as np
+
+from src.objects.CarlaSensor import CarlaSensor, CarlaSensorBuilder
+
+
+class TestCarlaSensor(unittest.TestCase):
+
+    def setUp(self):
+        self.carla_sensor = MagicMock()
+        self.carla_sensor.get_location = MagicMock(return_value=carla.Location(1.0, 2.0, 3.0))
+        self.carla_sensor.points_per_second = 1000
+        self.carla_sensor.rotation_frequency = 700
+        self.carla_sensor.horizontal_fov = 360
+        self.carla_sensor.upper_fov = 30
+        self.carla_sensor.lower_fov = -10
+
+    def test_builder(self):
+        sensor = CarlaSensorBuilder.build_sensor(self.carla_sensor)
+
+        assert sensor.carla_sensor == self.carla_sensor
+        assert (sensor.position == np.array([1.0, 2.0, 3.0])).all()
+        assert sensor.points_per_second == 1000
+        assert sensor.rotation_frequency == 700
+        assert sensor.horizontal_fov == np.deg2rad(360)
+        assert sensor.vertical_fov == np.deg2rad(40)
+
+
+if __name__ == "__main__":
+    unittest.main()
