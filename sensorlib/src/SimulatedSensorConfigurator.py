0880277464d414430cd15d5be0aef248a16d341b
David Gayman <david.gayman@leidos.com>
2023-09-24 16:51:55 -0400

    EGG finding code.

--- sensorlib/src/SimulatedSensorConfigurator.py
+++ sensorlib/src/SimulatedSensorConfigurator.py
@@ -6,7 +6,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 
+
+import glob
+import os
+import sys
+
+try:
+    sys.path.append(glob.glob('~/carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    exit()
 import carla
+
+
+
+
+
 import numpy as np
 
 from src.SemanticLidarSensor import SemanticLidarSensor
