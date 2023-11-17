Fix/orientation and covariance (#183)
<!-- Thanks for the contribution, this is awesome. -->

# PR Details
## Description
This PR:
- adds covariance to the object. Treated as none of the variables are
correlated to each other since pedestrian and vehicles can move randomly
(please confirm). Therefore, only their diagonal fields are updated
according to their standard deviation.
- fixes rotation where it is expected to be roll,pitch,yaw (shape 1,3)
in rest of the components but it is reporting rotation matrix (shape
3,3)
- velocity and angular velocity covariance is treated same as those of
position and orientation, which is the place I am not sure of.
<!--- Describe your changes in detail -->
Please see the github issue to see how the reported object's data
changed to see any errors or inconsistencies
EDIT: Not sure if I should add velocity and angular_velocity noises...
## Related GitHub Issue
https://github.com/usdot-fhwa-stol/carma-utils/issues/184
<!--- This project only accepts pull requests related to open GitHub
issues or Jira Keys -->
<!--- If suggesting a new feature or change, please discuss it in an
issue first -->
<!--- If fixing a bug, there should be an issue describing it with steps
to reproduce -->
<!--- Please DO NOT name partially fixed issues, instead open an issue
specific to this fix -->
<!--- Please link to the issue here: -->

## Related Jira Key
N/A
<!-- e.g. CAR-123 -->

## Motivation and Context
See above
<!--- Why is this change required? What problem does it solve? -->

## How Has This Been Tested?
Sim PC 1
<!--- Please describe in detail how you tested your changes. -->
<!--- Include details of your testing environment, and the tests you ran
to -->
<!--- see how your change affects other areas of the code, etc. -->

## Types of changes

<!--- What types of changes does your code introduce? Put an `x` in all
the boxes that apply: -->

- [X] Defect fix (non-breaking change that fixes an iss

sensorlib/src/noise_models/GaussianNoiseModel.py
@@ -19,8 +19,10 @@ class GaussianNoiseModel(AbstractNoiseModel):
 
     def __init__(self, config):
         self.__config = config
-        self.__position_std = self.__config["std_deviations"]["position"]
-        self.__orientation_std = self.__config["std_deviations"]["orientation"]
+        self.__position_std_in_meters = self.__config["std_deviations"]["position_in_meters"]
+        self.__orientation_std_in_radians = self.__config["std_deviations"]["orientation_in_radians"]
+        self.__linear_velocity_in_ms = self.__config["std_deviations"]["linear_velocity_in_ms"]
+        self.__angular_velocity_in_rs = self.__config["std_deviations"]["angular_velocity_in_rs"]
         self.__rng = np.random.default_rng()
 
     def apply_position_noise(self, object_list):
@@ -30,9 +32,8 @@ class GaussianNoiseModel(AbstractNoiseModel):
 
         # Apply position noise to the object_list
         noise_mean = 0.0
-        noise_std = self.__position_std
         for obj in object_list:
-            noise = np.random.normal(noise_mean, noise_std, size=3)
+            noise = np.random.normal(noise_mean, self.__position_std_in_meters, size=3)
             obj.position[0] += noise[0]
             obj.position[1] += noise[1]
             obj.position[2] += noise[2]
@@ -46,9 +47,9 @@ class GaussianNoiseModel(AbstractNoiseModel):
 
         # Apply orientation noise to the object_list
         noise_mean = 0.0
-        noise_std = self.__orientation_std
         for obj in object_list:
-            noise = np.random.normal(noise_mean, noise_std, size=3)
+            noise = np.random.normal(noise_mean, self.__orientation_std_in_radians, size=3)
+
             obj.rotation[0] += noise[0]
             obj.rotation[1] += noise[1]
             obj.rotation[2] += noise[2]
@@ -68,3 +69,51 @@ class GaussianNoiseModel(AbstractNoiseModel):
         if not self.__config["stages"]["list_inclusion_noise"]:
             return object_list
         return object_list[0:np.random.randint(0, len(object_list) + 1)]
+
+    def apply_position_covariance_noise(self, object_list):
+        if not self.__config["stages"]["position_noise"]:
+            return object_list
+
+        # Apply position noise to the object_list
+        for obj in object_list:
+            obj.position_covariance[0][0] = self.__position_std_in_meters[0] ** 2
+            obj.position_covariance[1][1] = self.__position_std_in_meters[1] ** 2
+            obj.position_covariance[2][2] = self.__position_std_in_meters[2] ** 2
+
+        return object_list
+
+    def apply_orientation_covariance_noise(self, object_list):
+        if not self.__config["stages"]["orientation_noise"]:
+            return object_list
+
+        # Apply orientation noise to the object_list
+        for obj in object_list:
+            obj.orientation_covariance[0][0] = self.__orientation_std_in_radians[0] ** 2
+            obj.orientation_covariance[1][1] = self.__orientation_std_in_radians[1] ** 2
+            obj.orientation_covariance[2][2] = self.__orientation_std_in_radians[2] ** 2
+
+        return object_list
+
+    def apply_linear_velocity_covariance_noise(self, object_list):
+        if not self.__config["stages"]["linear_velocity_noise"]:
+            return object_list
+
+        # Apply velocity noise to the object_list
+        for obj in object_list:
+            obj.velocity_covariance[0][0] = self.__linear_velocity_in_ms[0] ** 2
+            obj.velocity_covariance[1][1] = self.__linear_velocity_in_ms[1] ** 2
+            obj.velocity_covariance[2][2] = self.__linear_velocity_in_ms[2] ** 2
+
+        return object_list
+
+    def apply_angular_velocity_covariance_noise(self, object_list):
+        if not self.__config["stages"]["angular_velocity_noise"]:
+            return object_list
+
+        # Apply angular velocity noise to the object_list
+        for obj in object_list:
+            obj.angular_velocity_covariance[0][0] = self.__angular_velocity_in_rs[0] ** 2
+            obj.angular_velocity_covariance[1][1] = self.__angular_velocity_in_rs[1] ** 2
+            obj.angular_velocity_covariance[2][2] = self.__angular_velocity_in_rs[2] ** 2
+
+        return object_list

sensorlib/src/objects/DetectedObject.py
@@ -28,7 +28,9 @@ class DetectedObject:
     rotation: np.ndarray
     angular_velocity: np.ndarray
     position_covariance: np.ndarray
+    orientation_covariance: np.ndarray
     velocity_covariance: np.ndarray
+    angular_velocity_covariance: np.ndarray
     confidence: float
 
 
@@ -39,12 +41,12 @@ class DetectedObjectBuilder:
 
         if (object_type == "NONE"):
             return None
-        
+
         bounding_box = CarlaUtils.get_actor_bounding_box_points(carla_actor)
 
         if (bounding_box == None):
             return None
-        
+
         return DetectedObject(
             carla_actor,
             carla_actor.id,
@@ -53,11 +55,14 @@ class DetectedObjectBuilder:
             bounding_box,
             CarlaUtils.vector3d_to_numpy(carla_actor.get_location()),
             CarlaUtils.vector3d_to_numpy(carla_actor.get_velocity()),
-            CarlaUtils.get_actor_rotation_matrix(carla_actor),
+            CarlaUtils.get_actor_roll_pitch_yaw(carla_actor),
             CarlaUtils.get_actor_angular_velocity(carla_actor),
 
             # Use stand-in values which assume complete certainty
             np.zeros((3, 3)),
             np.zeros((3, 3)),
+            np.zeros((3, 3)),
+            np.zeros((3, 3)),
+
             1.0
         )

sensorlib/src/sensor/SemanticLidarSensor.py
@@ -15,7 +15,6 @@ from scipy.spatial import distance
 from objects.DetectedObject import DetectedObjectBuilder
 from sensor.SimulatedSensor import SimulatedSensor
 from util.CarlaUtils import CarlaUtils
-from util.HistoricalMapper import HistoricalMapper
 
 
 class SemanticLidarSensor(SimulatedSensor):
@@ -58,11 +57,6 @@ class SemanticLidarSensor(SimulatedSensor):
         self.__noise_model = noise_model
         self.__parent_id = parent_id
 
-        # Structures to store reassociation information
-        self.__actor_id_association = {}
-        trailing_id_associations_count = simulated_sensor_config[
-            "geometry_reassociation"]["trailing_id_associations_count"]
-        self.__trailing_id_associations = HistoricalMapper(trailing_id_associations_count)
         self.__rng = np.random.default_rng()
 
         # Object cache
@@ -297,7 +291,7 @@ class SemanticLidarSensor(SimulatedSensor):
 
         matching_nearest_neighbor_ids = self.compute_closest_object_id_list(hitpoints_in_map_frame, scene_objects,
                                                           self.__simulated_sensor_config["geometry_reassociation"][
-                                                              "geometry_association_max_distance_threshold"])
+                                                              "geometry_association_max_dist_in_meters"])
 
         association = zip(hitpoints, matching_nearest_neighbor_ids)
 
@@ -312,13 +306,13 @@ class SemanticLidarSensor(SimulatedSensor):
         # Filter unassociated hitpoints
         return grouped_data
 
-    def compute_closest_object_id_list(self, hitpoint_list, scene_objects, geometry_association_max_distance_threshold):
+    def compute_closest_object_id_list(self, hitpoint_list, scene_objects, geometry_association_max_dist_in_meters):
 
         """Get the closest objects to each hitpoint."""
-        return [self.compute_closest_object_id(hitpoint, scene_objects, geometry_association_max_distance_threshold) for
+        return [self.compute_closest_object_id(hitpoint, scene_objects, geometry_association_max_dist_in_meters) for
                 hitpoint in hitpoint_list]
 
-    def compute_closest_object_id(self, hitpoint, scene_objects, geometry_association_max_distance_threshold):
+    def compute_closest_object_id(self, hitpoint, scene_objects, geometry_association_max_dist_in_meters):
         """
         Compute the closest object to this hitpoint, which lies within a maximum distance threshold.
 
@@ -347,7 +341,7 @@ class SemanticLidarSensor(SimulatedSensor):
             geometry_association_threshold_buffer = 2.0
 
         # Observe a maximum object distance to preclude association with far-away objects
-        if distances[closest_index] > geometry_association_max_distance_threshold + geometry_association_threshold_buffer:
+        if distances[closest_index] > geometry_association_max_dist_in_meters + geometry_association_threshold_buffer:
             return None
 
         # Return closest object's ID
@@ -447,6 +441,11 @@ class SemanticLidarSensor(SimulatedSensor):
         detected_objects = self.__noise_model.apply_orientation_noise(detected_objects)
         detected_objects = self.__noise_model.apply_type_noise(detected_objects)
         detected_objects = self.__noise_model.apply_list_inclusion_noise(detected_objects)
+        detected_objects = self.__noise_model.apply_position_covariance_noise(detected_objects)
+        detected_objects = self.__noise_model.apply_orientation_covariance_noise(detected_objects)
+        detected_objects = self.__noise_model.apply_linear_velocity_covariance_noise(detected_objects)
+        detected_objects = self.__noise_model.apply_angular_velocity_covariance_noise(detected_objects)
+
         return detected_objects
 
     # ------------------------------------------------------------------------------

sensorlib/src/util/CarlaUtils.py
@@ -45,17 +45,16 @@ class CarlaUtils:
         return np.deg2rad(angular_velocity_degpersecond)
 
     @staticmethod
-    def get_actor_rotation_matrix(carla_actor):
+    def get_actor_roll_pitch_yaw(carla_actor):
         """
-        Get the rotation matrix for an actor.
+        Get the orientation for an actor in order of roll, pitch, yaw.
         :param carla_actor: The carla.Actor to obtain data from.
-        :return: numpy.array containing the rotation matrix in radians.
+        :return: numpy.array containing roll, pitch, yaw in radians.
         """
         carla_rotation = carla_actor.get_transform().rotation
         rotation_angles_deg = np.array([carla_rotation.roll, carla_rotation.pitch, carla_rotation.yaw])
         rotation_angles = np.deg2rad(rotation_angles_deg)
-        rotation_matrix = Rotation.from_euler('xyz', rotation_angles)
-        return rotation_matrix.as_dcm()
+        return rotation_angles
 
     @staticmethod
     def get_actor_bounding_box_points(carla_actor):
@@ -83,9 +82,9 @@ class CarlaUtils:
         """
         #using type_id instead of semantic_tags
         #issue with semantic_tags in version 0.9.10:https://github.com/carla-simulator/carla/issues/2161
-        
+
         temp_id_list =  carla_actor.type_id.split(".")
-        temp_id = temp_id_list[0] 
+        temp_id = temp_id_list[0]
         if temp_id == "vehicle":
             return "Vehicles"
         elif temp_id == "walker":