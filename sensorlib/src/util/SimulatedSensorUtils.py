70797645c9d54927ead8cf3ef662453cb9741a26
Eric Chen <122404059+EricChen-Lei@users.noreply.github.com>
2023-12-08 16:49:47 -0500

    Fix/timestamp to int (#199)
    <!-- Thanks for the contribution, this is awesome. -->
    
    # PR Details
    ## Description
    
    <!--- Describe your changes in detail -->
    CDAsim expects timestamp sent from sensorlib to be milliseconds in int
    format
    ## Related GitHub Issue
    
    <!--- This project only accepts pull requests related to open GitHub
    issues or Jira Keys -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please DO NOT name partially fixed issues, instead open an issue
    specific to this fix -->
    <!--- Please link to the issue here: -->
    
    ## Related Jira Key
    CDAR-537
    <!-- e.g. CAR-123 -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Defect fix (non-breaking change that fixes an issue)
    - [ ] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that cause existing functionality
    to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have added any new packages to the sonar-scanner.properties file
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have read the
    [**CONTRIBUTING**](https://github.com/usdot-fhwa-stol/carma-platform/blob/develop/Contributing.md)
    document.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.

--- sensorlib/src/util/SimulatedSensorUtils.py
+++ sensorlib/src/util/SimulatedSensorUtils.py
@@ -32,7 +32,7 @@ class DetectedObjectEncoder(json.JSONEncoder):
                 'angularVelocity': {'x': obj.angularVelocity[0], 'y': obj.angularVelocity[1], 'z': obj.angularVelocity[2]},
                 'angularVelocityCovariance': obj.angularVelocityCovariance.tolist(),
                 'size': {'length': obj.size[0], 'height': obj.size[1], 'width': obj.size[2]},
-                'timestamp': (int)(obj.timestamp), #TODO need to fix other instances in cdasim before changing https://github.com/usdot-fhwa-stol/carma-utils/issues/187
+                'timestamp': (int)((obj.timestamp)*1000), #CDASim expects Int in milliseconds
                 'sensorId': obj.sensorId
                 #'bounding_box_in_world_coordinate_frame': [array.tolist() for array in obj.bounding_box_in_world_coordinate_frame],
                 #'rotation': obj.rotation.tolist(),
