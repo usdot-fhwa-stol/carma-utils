f4a4a944455545c01a15098e0ec75f0fa593eb7d
John Stark <starkj@leidos.com>
2024-04-03 14:08:21 -0400

    Update system_alert publisher to transient local QoS

--- carma_ros2_utils/src/carma_lifecycle_node.cpp
+++ carma_ros2_utils/src/carma_lifecycle_node.cpp
@@ -75,8 +75,16 @@ namespace carma_ros2_utils
   CarmaLifecycleNode::on_configure(const rclcpp_lifecycle::State &prev_state)
   {
 
+    // NOTE: Currently, intra-process comms must be disabled for a publisher that is transient_local: https://github.com/ros2/rclcpp/issues/1753
+    rclcpp::PublisherOptions intra_proc_disabled; 
+    intra_proc_disabled.use_intra_process_comm = rclcpp::IntraProcessSetting::Disable;
+    
+    // Create a publisher that will send all previously published messages to late-joining subscribers ONLY If the subscriber is transient_local too
+    auto pub_qos_transient_local = rclcpp::QoS(rclcpp::KeepLast(5)); // A publisher with this QoS will store the last 5 messages that it has sent on the topic
+    pub_qos_transient_local.transient_local();
+
     system_alert_pub_ = create_publisher<carma_msgs::msg::SystemAlert>(
-        system_alert_topic_, 10);
+        system_alert_topic_, pub_qos_transient_local, intra_proc_disabled);
 
     return handle_on_configure(prev_state);
   }
